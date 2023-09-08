f599ed1d4ff1a03c8790b082c2796aff09d30a5a
Park HyoungJun <98789071+Asdfg00kl@users.noreply.github.com>
2023-09-08 17:09:57 +0900

    Update from tokenize import Number.py

--- from tokenize import Number.py
+++ from tokenize import Number.py
@@ -2,6 +2,8 @@ from tokenize import Number
 
 name = input("이름을 입력하세요: ")
 Number = input("학번을 입력하세요: ")
+major = input("학과를 입력하세요: ")
 print("<출력>")
 print("이름: ", name)
 print("학번: ", Number)
+print("학과: ", major)
